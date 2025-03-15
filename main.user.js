c0e348458be9dadf070ea10f695ea52751ceb2ac
willie <willie@goodjob.idv.tw>
2025-03-16 02:51:05 +0800

    模組化主程式 初始提交

---
+++ main.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        w3schools測驗增強工具
+// @namespace   Violentmonkey Scripts
+// @match       https://www.w3schools.com/*/exercise.asp*
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @version     0.1
+// @author      叮咚陳
+// @description 項目開始 - 2025/2/20 上午10:20:54
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  window.w3AutoHelper = {};
+})();
