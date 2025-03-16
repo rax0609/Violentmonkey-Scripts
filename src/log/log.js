296078236148515a65a60f667d0be9e3064401ad
willie <willie@goodjob.idv.tw>
2025-03-16 15:44:52 +0800

    重構日誌記錄功能，統一代碼風格並確保w3AutoHelper正確定義

--- src/log/log.js
+++ src/log/log.js
@@ -1,44 +1,46 @@
-// log.js
+(function () {
+  window.w3AutoHelper = window.w3AutoHelper || {};
 
-window.w3AutoHelper.logger = (function() {
+  window.w3AutoHelper.logger = (function () {
     class Logger {
-        constructor() {
-            this.logLevel = {
-                ERROR: 4,
-                WARN: 3,
-                INFO: 2,
-                DEBUG: 1
-            };
-            this.currentLevel = this.logLevel.INFO; // 預設等級
-        }
+      constructor() {
+        this.logLevel = {
+          ERROR: 4,
+          WARN: 3,
+          INFO: 2,
+          DEBUG: 1,
+        };
+        this.currentLevel = this.logLevel.INFO; // 預設等級
+      }
 
-        setLevel(level) {
-            this.currentLevel = this.logLevel[level];
-        }
+      setLevel(level) {
+        this.currentLevel = this.logLevel[level];
+      }
 
-        error(message, ...args) {
-            this._log('ERROR', message, ...args);
-        }
+      error(message, ...args) {
+        this._log("ERROR", message, ...args);
+      }
 
-        warn(message, ...args) {
-            this._log('WARN', message, ...args);
-        }
+      warn(message, ...args) {
+        this._log("WARN", message, ...args);
+      }
 
-        info(message, ...args) {
-            this._log('INFO', message, ...args);
-        }
+      info(message, ...args) {
+        this._log("INFO", message, ...args);
+      }
 
-        debug(message, ...args) {
-            this._log('DEBUG', message, ...args);
-        }
+      debug(message, ...args) {
+        this._log("DEBUG", message, ...args);
+      }
 
-        _log(level, message, ...args) {
-            if (this.logLevel[level] >= this.currentLevel) {
-                const timestamp = new Date().toISOString();
-                console.log(`[${timestamp}] [${level}] ${message}`, ...args);
-            }
+      _log(level, message, ...args) {
+        if (this.logLevel[level] >= this.currentLevel) {
+          const timestamp = new Date().toISOString();
+          console.log(`[${timestamp}] [${level}] ${message}`, ...args);
         }
+      }
     }
-    
+
     return new Logger();
+  })();
 })();
